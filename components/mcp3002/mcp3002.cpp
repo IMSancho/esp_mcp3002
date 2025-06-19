f90cb3bf532fde5b1fb5c2bec5a1a6c1a330dc33
IMSancho <dave.steele@gmail.com>
2025-06-19 23:47:57 +1000

    Update mcp3002.cpp

--- components/mcp3002/mcp3002.cpp
+++ components/mcp3002/mcp3002.cpp
@@ -31,7 +31,7 @@ float MCP3002::read_data(uint8_t pin) {
   data_null = this->transfer_byte(0x01);
 
   //data_msb = this->transfer_byte(command) & 0x03;
-  data_msb = this->transfer_byte(command);
+  data_msb = this->transfer_byte(command) & 0x1F;
   //data_msb = this->transfer_byte(0x0E) & 0x7F;
   data_lsb = this->transfer_byte(0x00);
 
